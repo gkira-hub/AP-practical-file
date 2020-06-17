c5c047a6bec9f9f21c9a7afbb2470f547a1317aa
gkira-hub <54792309+gkira-hub@users.noreply.github.com>
2020-06-17 20:03:03 +0530

    Add files via upload

---
+++ AP PRACTICALS/writing pyrhon.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 17 19:41:36 2020
+
+@author: gurkirat singh
+"""
+
+user_input = int(input('Please enter an integer between 1-5:> '))
+if user_input == 1:
+    print('one')
+elif user_input == 2:
+    print('two')
+elif user_input == 3:
+    print('three')
+elif user_input == 4:
+   print('four')
+elif user_input == 5:
+    print('five')
+else:
+    print('Out of range')
+
+
